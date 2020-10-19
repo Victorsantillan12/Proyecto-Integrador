afc7cfdebd8908b213538223e52020e0510a64c9
Victor Brackson Santillan Vasquez <victorsantillan@upeu.edu.pe>
2020-10-19 11:15:22 -0500

    subiendo los archivos

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ProyectoAngularxd app is running!');
+    expect(page.getTitleText()).toEqual('MOOC-IIAP app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProyectoAngularxd'`, () => {
+  it(`should have as title 'MOOC-IIAP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProyectoAngularxd');
+    expect(app.title).toEqual('MOOC-IIAP');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ProyectoAngularxd app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MOOC-IIAP app is running!');
   });
 });
